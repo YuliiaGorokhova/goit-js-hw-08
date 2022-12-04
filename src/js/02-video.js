1650987e38eb7cdfb3368a49b19f6eac5aa450b1
YuliiaGorokhova <Julia_gorokhova@ukr.net>
2022-12-04 23:05:43 +0200

    task_2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,17 +1,19 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
-const throttle = require('lodash.throttle');  //теж зробити імпортом
+// const throttle = require('lodash.throttle');
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('timeupdate', throttle(updatePosition, 1000));
-
 function updatePosition(data) {
   localStorage.setItem('videoplayer-current-time', data.seconds);
 }
+
+player.on('timeupdate', throttle(updatePosition, 1000));
+
 const currentTime = localStorage.getItem('videoplayer-current-time');
 
 if (currentTime) {
-  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+  player.setCurrentTime(currentTime);
 }
