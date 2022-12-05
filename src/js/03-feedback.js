e00bf54db60a087a356283a5c487401ca1d6a615
YuliiaGorokhova <Julia_gorokhova@ukr.net>
2022-12-05 19:58:03 +0200

    Task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,46 +1,38 @@
 import throttle from 'lodash.throttle';
 
-let feedbackDataStorage = {};
-
 const STORAGE_KEY = 'feedback-form-state';
 
-const refs = {
-  form: document.querySelector('.feedback-form'),
-  email: document.querySelector('input'),
-  textarea: document.querySelector('textarea'),
-  submit: document.querySelector('button'),
-};
+const formEl = document.querySelector('.feedback-form');
+const emailEl = document.querySelector('input');
+const messageEl = document.querySelector('textarea');
 
-refs.form.addEventListener('input', throttle(onInputHandler, 500));
-refs.form.addEventListener('submit', onSubmitHandler);
+formEl.addEventListener('input', throttle(inputHandler, 500));
+formEl.addEventListener('submit', submitHandler);
 
-function onInputHandler(event) {
-  const email = refs.email.value;
-  const message = refs.textarea.value;
+function inputHandler(evt) {
+  const email = emailEl.value;
+  const message = messageEl.value;
   localStorage.setItem(STORAGE_KEY, JSON.stringify({ email, message }));
-  //   feedbackDataStorage[event.target.name] = event.target.value;
-  //   localStorage.setItem(STORAGE_KEY, JSON.stringify(feedbackDataStorage));
 }
 
-const storagedData = localStorage.getItem(STORAGE_KEY);
-const storagedDataParsed = JSON.parse(storagedData);
+const storageParsed = JSON.parse(localStorage.getItem(STORAGE_KEY)); //можно зробити функцією
 
-if (storagedDataParsed) {
-  refs.email.value = storagedDataParsed.email;
-  refs.textarea.value = storagedDataParsed.message;
+if (storageParsed) {
+  emailEl.value = storageParsed.email;
+  messageEl.value = storageParsed.message;
 }
 
 console.log(localStorage);
 
-function onSubmitHandler(event) {
-  event.preventDefault();
-  const sentData = {
-    email: refs.email.value,
-    message: refs.textarea.value,
+function submitHandler(evt) {
+  evt.preventDefault();
+  const dataEl = {
+    email: emailEl.value,
+    message: messageEl.value,
   };
-  console.log(sentData);
-  refs.form.reset();
+  console.log(dataEl);
+  formEl.reset();
   localStorage.removeItem(STORAGE_KEY);
-  refs.email.value = '';
-  refs.textarea.value = '';
+  emailEl.value = '';
+  messageEl.value = '';
 }
